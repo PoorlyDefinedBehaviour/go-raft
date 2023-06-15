505cc1d91168b89b4ecfdc33352b60a4a6ef1006
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-06-15 20:25:03 -0300

    test Entry.IsHeartbeatEntry

--- src/types/types_test.go
+++ src/types/types_test.go
@@ -1,9 +1,32 @@
 package types
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestEntryIsHeartbeatEntry(t *testing.T) {
 	t.Parallel()
 
-	panic("todo")
+	cases := []struct {
+		description string
+		entry       Entry
+		expected    bool
+	}{
+		{
+			description: "entry is not a heartbeat entry, returns false",
+			entry:       Entry{Type: HeartbeatEntryType + 1},
+			expected:    false,
+		},
+		{
+			description: "entry is a heartbeat entry, returns true",
+			entry:       Entry{Type: HeartbeatEntryType},
+			expected:    true,
+		},
+	}
+
+	for _, tt := range cases {
+		assert.Equal(t, tt.expected, tt.entry.IsHeartbeatEntry(), tt.description)
+	}
 }
